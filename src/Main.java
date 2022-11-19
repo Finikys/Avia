fa4fc8906a3c6f8e533c50d6a42017125c5d4929
Finikys <foroncefinikys@gmail.com>
2022-11-19 14:11:03 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 2000;
+        int oneMiles = 20;
+        int miles = price / oneMiles;
+        System.out.print(miles + "Миль у вас есть");
+
+
+    }
+}
